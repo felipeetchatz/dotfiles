3bd7c7b57166576bd7455019746642c91c24285e
felipeetchatz <felipe.etchatz@gmail.com>
2023-01-23 19:22:09 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,13 @@
 
 cd "$HOME";
 
-sudo apt-get install git-core ctags zsh xclip
+sudo apt-get install git-core zsh
 
 echo "Cloning nandosousafr/dotfiles..."
-git clone https://github.com/nandosousafr/dotfiles.git;
+git clone https://github.com/felipeetchatz/dotfiles.git;
 
 echo "Setting up...";
 ln -s dotfiles/.gitignore_global .gitignore_global;
 rm ~/.zshrc && ln -s dotfiles/.zshrc .zshrc
 
 git config --global core.excludesfile ~/.gitignore_global;
-
-echo "Fetching neobundle...";
-
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh;
